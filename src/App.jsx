0487e3d5e3123aab52f4f5d734dd78b88cbd7d20
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-04-10 03:02:49 +0600

    2 api done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [data , setData] = useState([])
+  useEffect(() => {
+    fetch('fakeData.json')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+ 
   return (
     <div>
-
+      {
+         data.map(product => <img src={product.Company_Logo} />)
+      }
     </div>
   );
 };
